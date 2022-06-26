59ec859aa159f3650b0b115aacb88d2b99f9bb11
Jason Ferguson <fergusonjason@gmail.com>
2022-06-26 19:00:35 -0400

    show elapsed time

--- init.py
+++ init.py
@@ -1,5 +1,6 @@
 from neo4j_connection import Neo4jConnection
 import configparser
+import time
 
 def init(uri, user, password):
     conn = Neo4jConnection(uri, user, password)
@@ -8,6 +9,8 @@ def init(uri, user, password):
     conn.close()
 
 if __name__ == "__main__":
+
+    start_time = time.time()
     config = configparser.ConfigParser()
     config.read('app.properties')
 
@@ -20,3 +23,7 @@ if __name__ == "__main__":
     password = config['Database']['password']
 
     init(uri, user, password)
+
+    end_time = time.time()
+    total_time = round(end_time - start_time, 3)
+    print("Database truncated in {} seconds".format(total_time))
