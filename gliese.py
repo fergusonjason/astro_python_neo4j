3373915bdf9e0f305ebe08206d6f7effd4aca538
Jason Ferguson <fergusonjason@gmail.com>
2022-06-27 06:04:46 -0400

    commit buggy implementation

--- gliese.py
+++ gliese.py
@@ -3,8 +3,14 @@ from flamsteed import COLSPECS
 from neo4j_connection import Neo4jConnection
 from typing import List
 from typing import Tuple
+from typing import Dict
+from typing import Any
+from util import DMS2deg, HMS2deg
+import time
+import pandas as pd
+import configparser
 
-COLUMN_NAMES = ['Name','Comp','RAh','RAm', 'RAs','DE_','DEd','DEm','pm','pmPA','RV','Sp','VMag','BV','U_B','R_I','trplx','e_trplx','plx','e_plx','Mv','HD','DM','Giclas','LHS','OtherName']
+COLUMN_NAMES = ['Name','Comp','RAh','RAm', 'RAs','DE_','DEd','DEm','pm','pmPA','RV','Sp','VMag','BV','UB','RI','trplx','e_trplx','plx','e_plx','Mv','HD','DM','Giclas','LHS','OtherName']
 COLSPECS: List[Tuple[int,int]] = [(0,8),(8,10),(12,14),(15,17),(18,20),(21,22),(22,24),(25,29),(30,36),(37,42),(43,49),(54,66),(67,73),(75,80),(82,87),(89,94),
     (96,102),(102,107),(108,114),(114,119),(121,126),(146,152),(153,165),(168,175),(176,181),(182,187)]
 
@@ -12,7 +18,11 @@ CATALOG_NAME = 'Gliese'
 CATALOG_FULL_NAME = 'Preliminary Version of the Third Catalogue of Nearby Stars'
 CATALOG_AUTHOR = 'Gliese W., Jahreiss H.'
 
+
+
 def create_catalog(uri, user, password):
+
+    print("Gliese: Creating catalog")
     query_string = "MATCH (catalog: CATALOG {name: '$catalog_name'}) WITH COUNT(catalog) > 0 as node_exists RETURN node_exists"
     conn = Neo4jConnection(uri, user, password)
     result = conn.query(query_string)
@@ -29,10 +39,88 @@ def create_catalog(uri, user, password):
         conn.close()
     pass
 
-def import_entries(file_location: str):
-    pass
+def import_entries(uri: str, user:str, password:str, file_location: str):
+    print("Gliese: importing entries")
+    for chunk in pd.read_fwf(file_location, chunksize = 100, colspecs=COLSPECS, names=COLUMN_NAMES):
+        for row in chunk.itertuples():
+            #create dict
+            entry = convert_row_to_dict(row)
+            #write data to neo4j
+            query_string = "CREATE (s:STAR) set s = $entry return s"
+            conn = Neo4jConnection(uri, user, password)
+            response = conn.query(query_string, parameters={'entry': entry})
+            conn.close()
+            star = None
+            if response != None and response[0] != None:
+                star = response[0]
+
+            # create connection
+            query_string = "MATCH (c:CATALOG),(s:STAR) " \
+                "WHERE c.name='Gliese' and s.id = $id " \
+                "CREATE (s)-[ce:CATALOG_ENTRY] -> (c) " \
+                "return type(ce)"
+            response = conn.query(query_string, parameters={'id': star.get("id")})
+            conn.close()
+
+def convert_row_to_dict(row) -> Dict[str, Any]:
+
+    ra = '{} {} {}'.format(str(row.RAh), str(row.RAm), str(row.RAm))
+    ra_dec = HMS2deg(ra)
+    dec = "{} {} {}".format(str(row.DEd), str(row.DEm), str('0'))
+    
+    dec_dec = DMS2deg(dec)
+
+    result = {
+        'name': row.Name,
+        'component': row.Comp,
+        'epoch': 1950,
+        'ra': ra,
+        'ra_dec': ra_dec,
+        'dec': dec,
+        'dec_dec': dec_dec,
+        'proper motion': row.pm,
+        'proper motion direction': row.pmPA,
+        'radial velocity': row.RV,
+        'spectral type full': row.Sp, # this needs to be split into components
+        'visual magnitude': row.VMag,
+        'BV color': row.BV,
+        'UB color': row.UB,
+        'RI color': row.RI,
+        'trigonometric parallax': row.trplx,
+        'trigonometric parallax error margin': row.e_trplx,
+        'resulting_parallax': row.plx,
+        'resulting_parallax_error_margin': row.e_plx,
+        'absolute visual magnitude': row.Mv,
+        'HD number': row.HD,
+        'DM number': row.DM,
+        'Giclass': row.Giclass,
+        'LHS': row.LHS,
+        'Other name': row.OtherName
+    }
+
+    return result
 
 if __name__ == '__main__':
     if len(COLUMN_NAMES) != len(COLSPECS):
         print("length of column name {} does not match length of colspecs {}".format(len(COLUMN_NAMES), len(COLSPECS)))
+        quit()
+
+    start_time = time.time()
+
+    config = configparser.ConfigParser()
+    config.read('app.properties')
+
+    if config == None:
+        print("No config found")
+        quit()
+
+    uri = config['Database']['uri']
+    user = config['Database']['user']
+    password = config['Database']['password']
+
+    create_catalog(uri, user, password)
+    import_entries(uri, user, password, 'https://cdsarc.cds.unistra.fr/ftp/V/70A/catalog.dat.gz')
+    end_time = time.time()
+    total_time = round(end_time - start_time,3)
+    print("Gliese: catalog importing in {} seconds".format(total_time))
     pass
