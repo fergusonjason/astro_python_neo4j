fbfba0b9372d5d874fff9f36b52fe82908e3d6d2
Jason Ferguson <fergusonjason@gmail.com>
2022-07-02 16:04:35 -0400

    initial commit

---
+++ catalogs.py
@@ -0,0 +1,77 @@
+from neo4j_connection import Neo4jConnection
+import time
+from typing import Dict
+import configparser
+
+URI = ""
+USER = ""
+PASSWORD = ""
+
+def do_catalogs():
+
+    start_time = time.time()
+    print('Creating catalogs')
+
+    config = configparser.ConfigParser()
+    config.read('app.properties')
+
+    if config == None:
+        print("No config found")
+        quit()
+
+    URI = config['Database']['uri']
+    USER = config['Database']['user']
+    PASSWORD = config['Database']['password']
+
+    # create individual catalogs
+    catalogs = []
+    catalogs.append(create_gliese_dict())
+    catalogs.append(create_flamsteed_dict())
+
+    # write catalog nodes
+    query_string = "WITH $catalogs as catalogs " \
+        "UNWIND catalogs as catalog " \
+        "CREATE (c:CATALOG) set c += catalog " \
+        "RETURN c.name, id(c) as id"
+    conn = Neo4jConnection(db_uri=URI, user=USER, password=PASSWORD)
+    conn.query(query=query_string, parameters={'catalogs': catalogs})
+    conn.close()
+
+    # create index on name
+    query_string = "CREATE INDEX catalog_name_idx FOR (catalog:CATALOG) ON (catalog.name)"
+    conn = Neo4jConnection(db_uri=URI, user=USER, password=PASSWORD)
+    conn.query(query_string)
+
+    # create unique constraint on catalog name
+    query_string = "CREATE CONSTRAINT catalog_name_unique FOR (catalog:CATALOG) REQUIRE catalog.name IS UNIQUE";
+    conn = Neo4jConnection(db_uri=URI, user=USER, password=PASSWORD)
+    conn.query(query=query_string, parameters={'catalogs': catalogs})
+    conn.close()
+
+    end_time = time.time()
+    print("Created catalogs in {} seconds".format(end_time - start_time))
+
+def create_flamsteed_dict() -> Dict[str,str]:
+
+    return {
+        'name':'Flamsteed',
+        'catalog full name': '',
+        'catalog author': 'Flamsteed, Lalande'
+    }
+
+def create_gliese_dict() -> Dict[str, str]:
+
+    return {
+        'name': 'Gliese',
+        'catalog full name': 'Preliminary Version of the Third Catalogue of Nearby Stars',
+        'catalog author': 'Gliese W., Jahreiss H.'
+    }
+
+    # create the unique index
+
+def create_hd():
+    pass
+
+if __name__ == '__main__':
+    do_catalogs()
+
