471041c815ec74d55737b7417b7a3d9419a08f58
Jason Ferguson <fergusonjason@gmail.com>
2022-07-02 17:09:34 -0400

    initial commit

---
+++ create_database.py
@@ -0,0 +1,10 @@
+from init import do_init
+from catalogs import do_catalogs
+from flamsteed import do_flamsteed
+from gliese import do_gliese
+
+if __name__ == "__main__":
+    do_init()
+    do_catalogs()
+    do_flamsteed()
+    # do_gliese()
