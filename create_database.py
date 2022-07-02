dd5cc3c3b26d087b2de770299fc12f2b47219086
Jason Ferguson <fergusonjason@gmail.com>
2022-07-02 18:38:14 -0400

    update to refactored catalog package locations

--- create_database.py
+++ create_database.py
@@ -1,10 +1,12 @@
 from init import do_init
 from catalogs import do_catalogs
-from flamsteed import do_flamsteed
-from gliese import do_gliese
+from catalogs.flamsteed import do_flamsteed
+from catalogs.gliese import do_gliese
+from catalogs.hd import do_hd
 
 if __name__ == "__main__":
     do_init()
     do_catalogs()
     do_flamsteed()
     # do_gliese()
+    # do_hd()
