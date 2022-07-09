af245e1605748b05561711d29eba0ccb9fc87426
Jason Ferguson <fergusonjason@gmail.com>
2022-07-09 11:37:17 -0400

    fix configparser bug

--- spectral_types.py
+++ spectral_types.py
@@ -14,25 +14,14 @@ from config.config import URI, USER, PASSWORD
 def do_spectral_types():
     start_time = time.time()
 
-    config = configparser.ConfigParser()
-    config.read('app.properties')
-
-    if config == None:
-        print("No config found")
-        quit()
-
-    uri = config['Database']['uri']
-    user = config['Database']['user']
-    password = config['Database']['password']
-
-    make_spectral_types(uri, user=user, password=password)
-    create_unqiue_contraint(uri=uri, user=user, password=password)
+    make_spectral_types()
+    create_unqiue_contraint()
 
     end_time = time.time()
     total_time = end_time - start_time
-    print("Spectral types imported in {} seconds".format(round(total_time, 3)))
+    print("Spectral types created in {} seconds".format(round(total_time, 3)))
 
-def make_spectral_types(uri, user, password):
+def make_spectral_types():
 
     spectral_types = []
     for item in ['O','B','A','F','G','K','M']:
@@ -44,15 +33,13 @@ def make_spectral_types(uri, user, password):
     query_string = "WITH $spectral_types as spectral_types " \
         "UNWIND spectral_types as item " \
         "CREATE (st:SPECTRAL_TYPE) set st += item"
-    conn = Neo4jConnection(uri, user=user, password=password)
+    conn = Neo4jConnection(URI, USER, PASSWORD)
     result = conn.query(query_string, parameters={'spectral_types': spectral_types})
 
 
-
-
-def create_unqiue_contraint(uri, user, password):
+def create_unqiue_contraint():
     query_string = "CREATE CONSTRAINT spec_type_unique ON (st:SPECTRAL_TYPE) ASSERT st.spectral_type IS UNIQUE"
-    conn = Neo4jConnection(uri, user=user, password=password)
+    conn = Neo4jConnection(URI, USER, PASSWORD)
     result = conn.query(query=query_string)
 
 if __name__ == "__main__":
