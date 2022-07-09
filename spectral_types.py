34252e4e3278e19793ea93985dfac5f3c3179c76
Jason Ferguson <fergusonjason@gmail.com>
2022-07-08 20:18:52 -0400

    bring up to date with package locations

--- spectral_types.py
+++ spectral_types.py
@@ -1,6 +1,14 @@
 import configparser
 import time
-from catalogs.neo4j_connection.neo4j_connection import Neo4jConnection
+import os, sys, inspect
+
+# python rediculousness
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from neo4j_connection import Neo4jConnection
+from config.config import URI, USER, PASSWORD
 
 
 def do_spectral_types():
@@ -21,19 +29,24 @@ def do_spectral_types():
     create_unqiue_contraint(uri=uri, user=user, password=password)
 
     end_time = time.time()
-    print("Spectral types imported in {} seconds".format(end_time - start_time))
+    total_time = end_time - start_time
+    print("Spectral types imported in {} seconds".format(round(total_time, 3)))
 
 def make_spectral_types(uri, user, password):
 
-    spectral_types = ['O','B','F','G','K','M']
+    spectral_types = []
+    for item in ['O','B','A','F','G','K','M']:
+        node = {
+            "name": item
+        }
+        spectral_types.append(node)
 
     query_string = "WITH $spectral_types as spectral_types " \
-        "UNWIND spectral_types as spectral_type " \
-        "CREATE (st:SPECTRAL_TYPE) set st += spectral_type"
+        "UNWIND spectral_types as item " \
+        "CREATE (st:SPECTRAL_TYPE) set st += item"
     conn = Neo4jConnection(uri, user=user, password=password)
     result = conn.query(query_string, parameters={'spectral_types': spectral_types})
 
-    # for spectral_type in spectral_types:
 
 
 
@@ -41,7 +54,6 @@ def create_unqiue_contraint(uri, user, password):
     query_string = "CREATE CONSTRAINT spec_type_unique ON (st:SPECTRAL_TYPE) ASSERT st.spectral_type IS UNIQUE"
     conn = Neo4jConnection(uri, user=user, password=password)
     result = conn.query(query=query_string)
-    conn.close()
 
 if __name__ == "__main__":
     do_spectral_types()
