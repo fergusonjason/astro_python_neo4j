f971c23448070501e945c44c806a4357f6aee83a
Jason Ferguson <fergusonjason@gmail.com>
2022-07-09 11:20:15 -0400

    fixed configparser read issue

--- config/config.py
+++ config/config.py
@@ -1,11 +1,19 @@
 import configparser
+import os, sys, inspect
+
+# python rediculousness
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+
+filename = os.path.join(os.path.dirname(__file__), "app.properties")
+if not os.path.isfile(filename):
+    print("config/app.properties not found")
+    quit()
 
 config = configparser.ConfigParser()
-config.read('app.properties')
-
-if config == None:
-    print("No config found")
-    quit()
+config.read(filename)
 
 URI = config['Database']['uri']
 USER = config['Database']['user']
